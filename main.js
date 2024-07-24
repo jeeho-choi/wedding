fb11750e71e0b8848c2a57d1074a9cf05618f832
jeeho-choi <jeehochoi003@gmail.com>
2024-07-24 14:49:56 +0900

    add other sections

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-// main.js
-function showRSVP() {
-    document.getElementById('rsvp').style.display = 'block';
-}
+// RSVP.js
+// function showRSVP() {
+//     document.getElementById('rsvp').style.display = 'block';
+// }
 
-function hideRSVP() {
-    document.getElementById('rsvp').style.display = 'none';
-}
+// function hideRSVP() {
+//     document.getElementById('rsvp').style.display = 'none';
+// }
 
-document.getElementById('rsvp-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Thank you for your RSVP!');
-    hideRSVP();
-});
+// document.getElementById('rsvp-form').addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     alert('Thank you for your RSVP!');
+//     hideRSVP();
+// });
+
+
+
